70b7d40461b69d1c365f17b1bf1a7991e4d8541b
JoseRFelix <jfelx@outlook.com>
2020-12-09 22:33:42 -0400

    feat: add jest and test for Typography

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleDirectories: ['node_modules', __dirname],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/*.stories.{js,jsx,ts,tsx}',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+}
