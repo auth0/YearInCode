50ac900249a26683baa2433ff23c4f72b283dba0
JoseRFelix <jfelx@outlook.com>
2020-12-11 14:00:07 -0400

    feat: add Auth0 basic authentication with GitHub
    - Add simple smoke test

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -1,9 +1,22 @@
+import {buildUserProfile} from '../support/generate'
+
 describe('smoke', () => {
   it.only('should allow a typical user flow', () => {
+    const userProfile = buildUserProfile()
+    cy.intercept(`${Cypress.env('api_url')}/me`, {
+      statusCode: 200,
+      body: userProfile,
+    })
+    cy.intercept(`${Cypress.env('api_url')}/login`)
+
     cy.visit('/')
 
     cy.findByRole('heading', {name: 'Next.js and Auth0 Example'}).should(
       'exist',
     )
+    cy.findByText(new RegExp(`nickname: ${userProfile.nickname}`, 'i')).should(
+      'exist',
+    )
+    cy.findByText(new RegExp(`name: ${userProfile.name}`, 'i')).should('exist')
   })
 })
