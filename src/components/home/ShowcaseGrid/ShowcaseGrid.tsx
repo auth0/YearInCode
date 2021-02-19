0165bb35cf794ed6c43a1dd01fc3200acb405170
JoseRFelix <jfelx@outlook.com>
2021-02-19 14:17:37 -0400

    update: remove unused import

--- src/components/home/ShowcaseGrid/ShowcaseGrid.tsx
+++ src/components/home/ShowcaseGrid/ShowcaseGrid.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx'
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 
 import {Typography, Button} from '@components/ui'
 import GitHubIcon from '@assets/svg/github-logo.svg'
