2737b63bc3dac0f678e8aea63f047bb693cd298a
JoseRFelix <jfelx@outlook.com>
2021-02-20 02:09:20 -0400

    feat: add cache for gallery poster

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import * as cache from 'memory-cache'
+
 import {Footer, Header, Layout, PosterBackdrop} from '@components/common'
 import {Celebrate, Hero, ShowcaseGrid} from '@components/home'
 import {auth0} from '@lib/auth'
 import {PosterService} from '@lib/poster/poster-service'
 import {PosterGalleryResponse} from '@nebula/types/poster'
+import {logger} from '@nebula/log'
 
 interface HomeProps {
   isLoggedIn: boolean
@@ -29,15 +32,33 @@ export default function Home({isLoggedIn, galleryPosters}: HomeProps) {
 
 export async function getServerSideProps({req}) {
   const sessionPromise = auth0.getSession(req)
-  const posterImagesPromise = PosterService._getGalleryPosters()
+
+  const cachedGalleryPosters = cache.get('galleryPosters')
+  if (!cachedGalleryPosters) {
+    logger.info("Didn't find value in cache. Caching...")
+    const galleryPostersPromise = PosterService._getGalleryPosters()
+    const galleryPosters = await galleryPostersPromise
+
+    const session = await sessionPromise
+
+    cache.put('galleryPosters', galleryPosters)
+
+    return {
+      props: {
+        isLoggedIn: Boolean(session),
+        galleryPosters: galleryPosters,
+      },
+    }
+  }
+
+  logger.info('Found value in cache. Using it...')
 
   const session = await sessionPromise
-  const posterImages = await posterImagesPromise
 
   return {
     props: {
       isLoggedIn: Boolean(session),
-      galleryPosters: posterImages,
+      galleryPosters: cachedGalleryPosters,
     },
   }
 }
