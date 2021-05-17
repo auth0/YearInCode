a8bdf02fc48d97b213628f345260c24cd4dd4c21
Esteban Ibarra <eibarra1992@hotmail.com>
2021-05-17 15:40:33 -0500

    Add analytics script to the app

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -1,3 +1,7 @@
+import NextHead from 'next/head'
+
+import {constants} from '@lib/common'
+
 interface LayoutProps {
   navigation?: React.ReactNode
   content: React.ReactNode
@@ -5,11 +9,22 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({navigation, content, footer}) => (
-  <div className="container flex flex-col min-h-screen mx-auto">
-    {navigation}
-    <main className="flex flex-col flex-1">{content}</main>
-    {footer}
-  </div>
+  <>
+    <NextHead>
+      {constants.site.cloudflareAnalyticsId && (
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon={`{"token": "${constants.site.cloudflareAnalyticsId}"}`}
+        ></script>
+      )}
+    </NextHead>
+    <div className="container flex flex-col min-h-screen mx-auto">
+      {navigation}
+      <main className="flex flex-col flex-1">{content}</main>
+      {footer}
+    </div>
+  </>
 )
 
 export default Layout
--- src/lib/common/constants.ts
+++ src/lib/common/constants.ts
@@ -3,6 +3,7 @@ const constants = {
     url: process.env.NEXT_PUBLIC_POST_LOGOUT_REDIRECT_URI,
     cloudfront_url: process.env.NEXT_PUBLIC_CLOUDFRONT_URL,
     facebook_id: process.env.NEXT_PUBLIC_FACEBOOK_ID,
+    cloudflareAnalyticsId: process.env.NEXT_PUBLIC_CLOUDFLARE_ANALYTICS_ID,
   },
   auth0: {
     domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
