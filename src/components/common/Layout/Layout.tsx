4dbe3157f79a65889602202ec5dfdf6185452882
Esteban Ibarra <eibarra1992@hotmail.com>
2021-05-17 16:00:56 -0500

    Put script under `Head` component

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -1,7 +1,3 @@
-import NextHead from 'next/head'
-
-import {constants} from '@lib/common'
-
 interface LayoutProps {
   navigation?: React.ReactNode
   content: React.ReactNode
@@ -9,22 +5,11 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({navigation, content, footer}) => (
-  <>
-    <NextHead>
-      {constants.site.cloudflareAnalyticsId && (
-        <script
-          defer
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon={`{"token": "${constants.site.cloudflareAnalyticsId}"}`}
-        ></script>
-      )}
-    </NextHead>
-    <div className="container flex flex-col min-h-screen mx-auto">
-      {navigation}
-      <main className="flex flex-col flex-1">{content}</main>
-      {footer}
-    </div>
-  </>
+  <div className="container flex flex-col min-h-screen mx-auto">
+    {navigation}
+    <main className="flex flex-col flex-1">{content}</main>
+    {footer}
+  </div>
 )
 
 export default Layout
